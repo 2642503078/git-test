ad88506d9c86bfec30a33c84c071032c4dbf5e06
hui <2642503078@qq.com>
2023-05-13 06:03:36 +0800

    First Commit

---
+++ src/main/java/com/hui/GitTest.java
@@ -0,0 +1,13 @@
+package com.hui;
+
+/**
+ * Hello world!
+ *
+ */
+public class GitTest
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello git!" );
+    }
+}
